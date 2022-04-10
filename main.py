089bf0091c3e09d72a52e0d1ed42e23cbbf6fcec
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-10 16:04:13 -0300

    Implemented graph plot

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from kivy.clock import Clock
 import pandas as pd
 from matplotlib import pyplot as plt
 import math
+# execute in prompt: garden install matplotlib
+
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
 Window.size = (1000, 600)
 
@@ -58,19 +61,44 @@ class Interface(BoxLayout):
         self.ids.upload_btn.source = "Drag.png"
 
     def update(self):
+        flatten_axis = list()
+        # Checking for Childrens
+        children = self.ids.graph_placeholder.children
+        if children:
+            self.ids.graph_placeholder.remove_widget(children[0])
         # Total number of charts
         files_len = len(self.files)
-        row_col = math.ceil(files_len/2)
-        fig, axis = plt.subplots(row_col, row_col)
+        if files_len == 1:
+            fig, axis = plt.subplots(1, 1)
+            flatten_axis.append(axis)
+        elif files_len == 2:
+            fig, axis = plt.subplots(2, 1)
+            flatten_axis = axis.flatten
+        else:
+            row_col = math.ceil(files_len / 2)
+            fig, axis = plt.subplots(row_col, row_col)
+            flatten_axis = axis.flatten
+
         # keys
         files_checkbox = self.obj_dict.keys()
-        for file_checkbox in files_checkbox:
+        properties_checkbox = self.column_dict.keys()
+        plt.gcf().autofmt_xdate()
+        for index, file_checkbox in enumerate(files_checkbox):
+            y_axis = list()
             if file_checkbox.active:
                 file_name = self.obj_dict[file_checkbox]
                 file_address = self.files_address[file_name]
                 content = pd.read_excel(file_address)
                 print(content)
-
+                for property_checkbox in properties_checkbox:
+                    if property_checkbox.active:
+                        if self.column_dict[property_checkbox] == "Cargo":
+                            x_axis = content[self.column_dict[property_checkbox]]
+                        else:
+                            y_axis.append(content[self.column_dict[property_checkbox]])
+                for y in y_axis:
+                    flatten_axis[index].plot(x_axis.to_numpy(), y.to_numpy())
+        self.ids.graph_placeholder.add_widget(FigureCanvasKivyAgg(plt.gcf()))
 
     def upload_menu(self):
         self.ids.upload_btn.source = "Drop.png"
@@ -78,6 +106,10 @@ class Interface(BoxLayout):
 
     def switching(self):
         self.ids.sm.current = "visualizer_window"
+
+    def saving(self):
+        dir = filechooser.choose_dir(title="Please Select A Location")
+        plt.savefig(dir[0]+"\\Figure.png")
 
 
 class VisualizerApp(App):
